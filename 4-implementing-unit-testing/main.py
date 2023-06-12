51f33626b4d2640a0c8cd7fe175c6c01d0eae301
Allan Robert Tan <argt@duck.com>
2023-06-13 07:48:45 +0800

    add more detail

--- 4-implementing-unit-testing/main.py
+++ 4-implementing-unit-testing/main.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 from emails import find_email
 import sys
+import re
+
+
+def is_email_address(string):
+    pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    return re.match(pattern, string) is not None
 
 
 def main():
     email_address = find_email(sys.argv)
-    print(f'Email Address: {email_address}')
+
+    print(f'Email address: {email_address}' if is_email_address(
+        email_address) else email_address)
 
 
 if __name__ == '__main__':
